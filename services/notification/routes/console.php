de4d4945a34bb460ff8a99fd2b303a53f5e0054d
E-jay Detera <edetera41@gmail.com>
2026-06-07 22:33:46 +0800

    feat: initialize notification service configuration and add mail test artisan command

--- services/notification/routes/console.php
+++ services/notification/routes/console.php
@@ -6,3 +6,17 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('mail:test {email}', function (string $email) {
+    $this->info("Sending test email to {$email}...");
+    
+    try {
+        \Illuminate\Support\Facades\Mail::raw('This is a test email sent from CRMS Capstone using Brevo!', function ($message) use ($email) {
+            $message->to($email)
+                    ->subject('CRMS Brevo SMTP Test');
+        });
+        $this->info("Test email successfully dispatched!");
+    } catch (\Exception $e) {
+        $this->error("Failed to send email: " . $e->getMessage());
+    }
+})->purpose('Send a test email using configured SMTP credentials');
