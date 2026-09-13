cfe4e24374600957d542163670a1cfe953a323c1
shadrack <castroshadrack7@gmail.com>
2026-09-13 17:40:15 +0800

    chore: remove unused packages and table boilerplate

--- frontend/web/src/components/ui/table/utils.ts
+++ frontend/web/src/components/ui/table/utils.ts
@@ -1,10 +1,9 @@
-import type { Updater } from "@tanstack/vue-table"
-
 import type { Ref } from "vue"
-import { isFunction } from "@tanstack/vue-table"
+
+export type Updater<T> = T | ((old: T) => T)
 
 export function valueUpdater<T>(updaterOrValue: Updater<T>, ref: Ref<T>) {
-  ref.value = isFunction(updaterOrValue)
-    ? updaterOrValue(ref.value)
+  ref.value = typeof updaterOrValue === "function"
+    ? (updaterOrValue as (old: T) => T)(ref.value)
     : updaterOrValue
 }
